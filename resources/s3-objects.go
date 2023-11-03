5a10b3c555a3c7c20d926bb083c187766184b47e
Corentin <corentin.muller@converteo.com>
2023-11-03 09:54:07 +0100

    [ADD] datetocheck

--- resources/s3-objects.go
+++ resources/s3-objects.go
@@ -108,11 +108,11 @@ func (e *S3Object) Remove() error {
 
 func (e *S3Object) Properties() types.Properties {
 	currentDate := time.Now()
-	daysDifference := currentDate.Sub(dateToCheck).Hours() / 24
+	daysDifference := currentDate.Sub(e.creationDate).Hours() / 24
 	resource_age := "is younger than 30 days"
 	// Check if the date is older than 30 days
 	if daysDifference > 30 {
-		resource_age := "is older than 30 days"
+		resource_age = "is older than 30 days"
 	return types.NewProperties().
 		Set("Age", resource_age).
 		Set("Bucket", e.bucket).
