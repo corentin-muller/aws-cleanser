0a9027ee16842d8f6642b12604e33720d734b1ee
Corentin <corentin.muller@converteo.com>
2023-11-09 11:33:08 +0100

    [ADD] code to update config file

--- update-config/lambda_function.py
+++ update-config/lambda_function.py
@@ -2,6 +2,8 @@ import boto3
 import json
 import yaml
 
+{'account-blocklist': [123456789012], 'accounts': {'ACCOUNT': {'filters': {'S3Bucket': [{'invert': 'true', 'type': 'exact', 'value': 'nuke-account-cleanser-config-930842625961-eu-west-3-4519c290'}]}}}, 'regions': ['eu-west-3'], 'resource-types': {'targets': ['S3Bucket']}}
+
 S3_OBJECT = "nuke_generic_config.yaml"
 S3_BUCKET = "nuke-account-cleanser-930842625961-eu-west-3-41d1db50"
 
@@ -9,6 +11,7 @@ def lambda_handler(event, context):
     print("start")
     s3 = boto3.client("s3")
     resource = event["resource"]
+    identifier = event["name"]
       
     try:
         # Download the file from S3
@@ -21,6 +24,20 @@ def lambda_handler(event, context):
             return exc
     except Exception as e:
         print(f"Error: {e}")
+    filter_resource = configfile["accounts"]["ACCOUNT"]["filters"]
+    if resource in filter_resource.keys():
+        to_append = {
+                "invert": "false",
+                "type": "exact",
+                "value": identifier
+            }
+        filter_resource[resource].append(to_append)
+    else:
+        configfile["accounts"]["ACCOUNT"]["filters"][resource] = {
+                "invert": "false",
+                "type": "exact",
+                "value": identifier
+        }
         
     return {
         'statusCode': 200,
