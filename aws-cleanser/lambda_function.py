8846a643266b2939045e1cd9bdca376883383508
Corentin <corentin.muller@converteo.com>
2023-11-14 11:41:40 +0100

    [FIX] reformat python code

--- aws-cleanser/lambda_function.py
+++ aws-cleanser/lambda_function.py
@@ -5,11 +5,16 @@ import yaml
 S3_OBJECT = "nuke_delete.yaml"
 S3_BUCKET = "nuke-account-cleanser-930842625961-eu-west-3-41d1db50"
 
+
 def lambda_handler(event, context):
-    print("start")
-    configfile = {
-        "account-blocklist": ["999999999999"]
-    }
+    """
+    Trigger step function to delete provided resource
+    :param event
+    :param context
+
+    :returns: 0
+    """
+    configfile = {"account-blocklist": ["999999999999"]}
     s3 = boto3.client("s3")
 
     region_clean = event.get("region", "eu-west-3")
@@ -17,32 +22,32 @@ def lambda_handler(event, context):
     service_name = event["name"]
     dry_run = event.get("dry_run", "true")
     version = event.get("version", "8.1")
-    
+
     configfile["regions"] = [region_clean]
     configfile["resource-types"] = {"targets": [resource_clean]}
     configfile["accounts"] = {
         "ACCOUNT": {
             "filters": {
-                resource_clean: [{'type': "exact", 'value': service_name, 'invert': 'true'}]
+                resource_clean: [
+                    {"type": "exact", "value": service_name, "invert": "true"}
+                ]
             }
         }
     }
     print(configfile)
     config_output = yaml.dump(configfile)
-    s3.put_object(
-        Bucket=S3_BUCKET, 
-        Key=S3_OBJECT,
-        Body=config_output
-    )
-    sf = boto3.client('stepfunctions', region_name = 'eu-west-3')
-    input_dict = {"InputPayLoad": {
-        "invocation_type": "lambda",
-        "nuke_dry_run": dry_run,
-        "nuke_version": version,
-        "region_list": [region_clean]
+    s3.put_object(Bucket=S3_BUCKET, Key=S3_OBJECT, Body=config_output)
+    sf = boto3.client("stepfunctions", region_name="eu-west-3")
+    input_dict = {
+        "InputPayLoad": {
+            "invocation_type": "lambda",
+            "nuke_dry_run": dry_run,
+            "nuke_version": version,
+            "region_list": [region_clean],
         }
     }
     response = sf.start_execution(
-    stateMachineArn = 'arn:aws:states:eu-west-3:930842625961:stateMachine:aws-account-clean-codebuild-state-machine',
-    input = json.dumps(input_dict))
+        stateMachineArn="arn:aws:states:eu-west-3:930842625961:stateMachine:aws-account-clean-codebuild-state-machine",
+        input=json.dumps(input_dict),
+    )
     return 0
--- update-config/lambda_function.py
+++ update-config/lambda_function.py
@@ -6,18 +6,26 @@ import yaml
 S3_OBJECT = "nuke_generic_config.yaml"
 S3_BUCKET = "nuke-account-cleanser-930842625961-eu-west-3-41d1db50"
 
+
 def lambda_handler(event, context):
+    """
+    Updates the nuke_generic_config.yaml file located on s3 with resource provided
+    :param event
+    :param context
+
+    :returns: status
+    """
     print("start")
     s3 = boto3.client("s3")
     resource = event["resource"]
     identifier = event["name"]
-      
+
     try:
         # Download the file from S3
         response = s3.get_object(Bucket=S3_BUCKET, Key=S3_OBJECT)
 
         try:
-            configfile = yaml.safe_load(response['Body'])
+            configfile = yaml.safe_load(response["Body"])
             print(configfile)
         except yaml.YAMLError as exc:
             print(exc)
@@ -27,24 +35,12 @@ def lambda_handler(event, context):
 
     filter_resource = configfile["accounts"]["ACCOUNT"]["filters"]
     if resource in filter_resource.keys():
-        to_append = {
-                "type": "exact",
-                "value": identifier
-                }
+        to_append = {"type": "exact", "value": identifier}
         filter_resource[resource].append(to_append)
     else:
-        configfile["accounts"]["ACCOUNT"]["filters"][resource] = [{
-                "type": "exact",
-                "value": identifier
-        }
-    ]
+        configfile["accounts"]["ACCOUNT"]["filters"][resource] = [
+            {"type": "exact", "value": identifier}
+        ]
     config_output = yaml.dump(configfile)
-    s3.put_object(
-        Bucket=S3_BUCKET, 
-        Key=S3_OBJECT,
-        Body=config_output
-    )
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    s3.put_object(Bucket=S3_BUCKET, Key=S3_OBJECT, Body=config_output)
+    return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
